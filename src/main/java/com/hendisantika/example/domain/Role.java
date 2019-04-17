899513d3bb6c3d941fb002e94a64b71bf27446cd
vikramwable <vikramatc@gmail.com>
2019-04-17 12:23:46 +0530

    First project Skeleton commit

---
+++ src/main/java/com/hendisantika/example/domain/Role.java
@@ -0,0 +1,7 @@
+package com.hendisantika.example.domain;
+
+public enum Role {
+
+  USER, ADMIN
+
+}
---
+++ src/main/java/com/hendisantika/example/service/user/UserService.java
@@ -0,0 +1,19 @@
+package com.hendisantika.example.service.user;
+
+import com.hendisantika.example.domain.User;
+import com.hendisantika.example.domain.UserCreateForm;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserService {
+
+  Optional<User> getUserById(long id);
+
+  Optional<User> getUserByEmail(String email);
+
+  Collection<User> getAllUsers();
+
+  User create(UserCreateForm form);
+
+}
